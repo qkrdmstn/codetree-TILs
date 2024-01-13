454d8cc51be0fbfb4e98464b30b14dfeb7eba5f8
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-13 11:34:48 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240113/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    //변수 선언
+    int a = 1, b = 2, c = 3;
+    int sum = a + b + c;
+
+    //합 복사
+    a = sum;
+    b = sum;
+    c = sum;
+
+    //출력
+    printf("%d %d %d", a, b, c);
+    return 0;
+}
