68fd6659d2b161154212c3796894c48aaaef1db5
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-13 13:04:40 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240113/살 수 있는 물건/things-able-to-buy.cpp
+++ 240113/살 수 있는 물건/things-able-to-buy.cpp
@@ -4,9 +4,9 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    if(n>3000)
+    if(n>=3000)
         printf("book");
-    else if(n>1000)
+    else if(n>=1000)
         printf("mask");
     else
         printf("no");
