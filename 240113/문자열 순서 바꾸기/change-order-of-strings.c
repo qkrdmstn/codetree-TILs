e2b5b3e21f55474ff1a42417dda48f472af19c9c
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-13 12:00:18 +0900

    Title: 문자열 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240113/문자열 순서 바꾸기/change-order-of-strings.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    //변수 선언
+    char s[100];
+    char t[100];
+
+    //입력
+    scanf("%s %s", s, t);
+
+    //출력
+    printf("%s\n%s", t, s);
+    return 0;
+}
