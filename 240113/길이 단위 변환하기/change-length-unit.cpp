0e4b56b29bb642f92c56dce2b8cf668787c70194
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-13 11:17:24 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240113/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a = 9.2;
+    double a1 = 30.48;
+    double b = 1.3;
+    double b1 = 160934;
+
+    printf("%.1fft = %.1fcm\n", a,a*a1);
+    printf("%.1fmi = %.1fcm", b,b*b1);
+    return 0;
+}
