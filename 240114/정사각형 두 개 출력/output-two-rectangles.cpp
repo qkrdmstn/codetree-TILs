07ca411fe72bcf0ade2baceffe0ee489066a5aef
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-14 17:54:34 +0900

    Title: 정사각형 두 개 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240114/정사각형 두 개 출력/output-two-rectangles.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for(int i=0; i<2; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            for(int k=0; k<n; k++)
+                printf("*");
+            printf("\n");
+        }
+        printf("\n");
+    }
+    return 0;
+}
