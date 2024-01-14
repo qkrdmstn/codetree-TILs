965efe5a3cf2b1c5e2da5c9fdd1a71fb11af93d1
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-15 00:26:21 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for(int i=0; i<2*n; i++)
+    {
+        if((i+1)%2 == 1)
+        {
+            for(int j=0; j<n-(i+1)/2; j++)
+                printf("* ");
+        }
+        else
+        {
+            for(int j=0; j<(i+1)/2; j++)
+                printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
