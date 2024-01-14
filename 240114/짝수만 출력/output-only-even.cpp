8c5057220da80cdbb1ec34cd2489757af99f1c68
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-14 16:49:29 +0900

    Title: 짝수만 출력, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240114/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    for(int i=a; i<=b; i++)
+    {
+        if(i%2==0)
+            printf("%d ", i);
+    }
+    return 0;
+}
