56bf2e20e190e622bf3c3860421f18e51ae38e05
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-15 11:41:21 +0900

    Title: 구구단으로 이루어진 직각 삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n-i+1; j++)
+        {
+            printf("%d * %d = %d", i, j, i*j);
+            if(j!=n-i+1)
+                printf(" / ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
