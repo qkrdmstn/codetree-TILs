6cc3c5240f6630004e723c1c57e9fc19e98d99ff
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-15 12:13:25 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240115/2가 3번째로 등장하는 위치/where-2-appears-3rd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int n, cnt=0, index;
+    int a[100];
+
+    scanf("%d", &n);
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+        if(a[i] == 2)
+            cnt++;
+        if(cnt==3)
+            index = i;
+    }
+
+    printf("%d", index - 1);
+    return 0;
+}
