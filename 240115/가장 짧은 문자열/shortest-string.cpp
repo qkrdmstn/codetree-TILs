d9464b3704f8b61a4f3d335755a5d22421b5036f
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-15 13:16:54 +0900

    Title: 가장 짧은 문자열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240115/가장 짧은 문자열/shortest-string.cpp
+++ 240115/가장 짧은 문자열/shortest-string.cpp
@@ -8,12 +8,12 @@ int main() {
 
     int max, min;
     max = a.length();
-    if(max > b.length()) max = b.length();
-    if(max > c.length()) max = c.length();
+    if(max < b.length()) max = b.length();
+    if(max < c.length()) max = c.length();
    
     min = a.length();
-    if(min < b.length()) min = b.length();
-    if(min < c.length()) min = c.length();
+    if(min > b.length()) min = b.length();
+    if(min > c.length()) min = c.length();
     
     cout << max - min;
     return 0;
