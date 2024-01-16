05af7f994deb76b9802bedc28877a412d87eec0a
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-16 10:53:29 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240116/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int n, cnt=0;
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<i; j++)
+            printf("  ");
+        for(int j=0; j<n-i; j++)
+        {
+            printf("%c ", 'A'+ cnt);
+            cnt++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
