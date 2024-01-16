beb78b3e9ba155c632cad73d8eff2235a3b4bcca
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-16 15:54:13 +0900

    Title: 대문자 소문자 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240116/대문자 소문자 바꾸기/change-uppercase-and-lowercase.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;
+    
+    for(int i=0; i<a.length(); i++)
+    {
+        if(a[i] >= 65 && a[i] <= 90)
+            a[i] += 32;
+        else
+            a[i] -=32;
+    }
+
+    cout << a;
+    return 0;
+}
