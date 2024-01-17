5accdbac646af5beb967adffbe6ec92b0ff4cbfc
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-17 17:20:15 +0900

    Title: 숫자로 이루어진 사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240117/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
+++ 240117/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 void PrintNum(int n)
 {
-    int cnt=0;
+    int cnt=1;
     for(int i=0; i<n; i++)
     {
         for(int j=0; j<n; j++)
         {
-            cout <<cnt;
+            cout <<cnt <<  " ";
             cnt++;
+            if(cnt > 9)
+                cnt=1;
         }
         cout << endl;
     }
@@ -19,6 +21,6 @@ int main() {
     int n;
     cin >> n;
 
-    PrintNum(n)
+    PrintNum(n);
     return 0;
 }
