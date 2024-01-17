3e02c79159fc20cfd42e92a8caca8bbfb9a7b7cf
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-17 17:11:54 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240117/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void PrintStr(int n)
+{
+    for(int i=0; i<n; i++)
+        cout<<"12345^&*()_"<<endl;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    PrintStr(n);
+    return 0;
+}
