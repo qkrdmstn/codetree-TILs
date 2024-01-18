a6849c9590bf6687d51fc13f97dc7bdd31f70198
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-18 17:43:03 +0900

    Title: 두 정수 값 교환하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240118/두 정수 값 교환하기/to-exchange-two-integer-values.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void Swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    Swap(a, b);
+    
+    cout << a << b;
+    return 0;
+}
