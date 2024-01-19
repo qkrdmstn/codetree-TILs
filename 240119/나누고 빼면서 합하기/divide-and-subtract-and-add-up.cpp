7c53c8a70fc6d8ca371b0a2cd08eb9c0256c95cf
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-19 13:49:31 +0900

    Title: 나누고 빼면서 합하기, Time: 13ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240119/나누고 빼면서 합하기/divide-and-subtract-and-add-up.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[100];
+
+int func()
+{
+    int sum=0;
+    while(m!=1)
+    {
+        sum += arr[m];
+        if(m%2 == 0)
+            m/=2;
+        else
+            m-=1;
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+        
+    func();
+
+    for(int i=0; i<n; i++)
+        cout << arr[i];
+    return 0;
+}
