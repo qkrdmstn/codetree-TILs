f0a131520c34318810cf12ccfc21f9d786023034
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-19 14:06:58 +0900

    Title: 반복 출력하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240119/반복 출력하기 2/repeated-output-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void Print(int n)
+{
+    if(n==0)
+        return;
+    
+    cout << "HelloWorld\n"
+    Print(n-1);
+}
+int main() {
+    int n;
+    cin >> n;
+    
+    Print(n);
+    return 0;
+}
