de3b40b125eba50db6c3aa43cd9e31d520954151
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-19 14:07:24 +0900

    Title: 반복 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240119/반복 출력하기 2/repeated-output-2.cpp
+++ 240119/반복 출력하기 2/repeated-output-2.cpp
@@ -6,13 +6,13 @@ void Print(int n)
     if(n==0)
         return;
     
-    cout << "HelloWorld\n"
+    cout << "HelloWorld\n";
     Print(n-1);
 }
 int main() {
     int n;
     cin >> n;
-    
+
     Print(n);
     return 0;
 }
