534d7678097340de80e7a467329574bc4dad4fa0
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-21 16:12:04 +0900

    Title: 오름 내림차순 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240121/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    int arr[100];
+
+    cin >> n;
+    for (int i=0; i<n; i++)
+        cin >> arr[i];
+    
+    sort(arr, arr + n);
+
+    for(int i=0; i<n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
+    for(int i=n-1; i>=0; i--)
+        cout << arr[i] << " ";
+    return 0;
+}
