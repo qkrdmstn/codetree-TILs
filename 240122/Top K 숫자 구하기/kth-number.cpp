741c9596241cd74d95d5e49275581823a93c165d
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-22 11:26:59 +0900

    Title: Top K 숫자 구하기, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240122/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, k;
+    int a[100];
+
+    cin >> n >> k;
+    for(int i=0; i<n; i++)
+        cin >> a[i];
+
+    sort(a, a + n);
+
+    cout << a[k-1];
+    return 0;
+}
