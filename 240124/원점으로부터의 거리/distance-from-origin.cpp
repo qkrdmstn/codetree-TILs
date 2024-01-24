78f22c44ad6deec44732f2c5f06a1ea1b04af2f7
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-24 17:04:20 +0900

    Title: 원점으로부터의 거리, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240124/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Point
+{
+public:
+int x, y;
+int dist;
+int num;
+
+public:
+Point(int _x = 0, int _y = 0, int _num = -1)
+{
+    x = _x;
+    y = _y;
+    dist = x + y;
+    num = _num;
+}
+
+void Print()
+{
+    cout << num << endl;
+}
+};
+
+bool Compare(Point a, Point b)
+{
+    if(a.dist == b.dist)
+        return a.num < b.num;
+    return a.dist < b.dist;
+}
+
+int main() {
+    int n;
+    Point points[1000];
+
+    cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        int _x, _y;
+        cin >> _x >> _y;
+
+        points[i] = Point(_x, _y, i+1);
+    }
+
+    sort(points, points + n, Compare);
+
+    for(int i=0; i<n; i++)
+        points[i].Print();
+    return 0;
+}
