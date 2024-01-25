d35d44b47807d69e290c51430665cd03996586fb
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-25 13:46:01 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240125/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int d = a-11;
+    int h = d*24 + (b-11);
+    int m = h*60 + (c-11);
+
+    cout << m;
+
+ 
+    return 0;
+}
