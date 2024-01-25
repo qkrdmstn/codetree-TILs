5f28f50492243b5a988776e86d2048a20175cc46
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-25 15:54:45 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240125/요일 맞추기/guess-day-of-week.cpp
+++ 240125/요일 맞추기/guess-day-of-week.cpp
@@ -10,8 +10,17 @@ int main() {
     int day_of_month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     int days = 0;
-    for(int i=m1; i<m2; i++)
-        days += day_of_month[i];
+    if(m1 <= m2)
+    {
+        for(int i=m1; i<m2; i++)
+            days += day_of_month[i];
+    }
+    else
+    {
+        for(int i=m2; i<m1; i++)
+            days += day_of_month[i];
+    }
+    
     days += d2 - d1;
     
     if(days % 7 < 0)
