04ae82aed999191191ed4099cf5ca0cdefeb47fc
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-25 16:10:26 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240125/요일 맞추기/guess-day-of-week.cpp
+++ 240125/요일 맞추기/guess-day-of-week.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main() {
-    int m1, d1, m2, d2;
-    cin >> m1 >> d1 >> m2 >> d2;
+    int m1 = 5, d1 = 11, m2 = 4, d2 = 26;
+    //cin >> m1 >> d1 >> m2 >> d2;
 
     string week[7] = {"Mon", "Tue", "Wed" , "Thu", "Fri","Sat", "Sun"};
     int day_of_month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -19,10 +19,10 @@ int main() {
     else
     {
         for(int i=m2; i<m1; i++)
-            days += day_of_month[i];
-        days += d1 - d2;
+            days -= day_of_month[i];
+        days -= d1 - d2;
     }
-    
+
     if(days % 7 < 0)
         days = 7 + (days%7);
 
