d3a033727b3d58c578188a5157c556f0859b5308
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-26 16:53:20 +0900

    Title: 진수 to 진수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/진수 to 진수/transformation-of-number-system.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //변수 선언
+    int a, b;
+    string n;
+
+    //입력
+    cin >> a >> b;
+    cin >> n;
+
+    //a진수 -> 10진수
+    int cnt = 1;
+    int decimal = 0;
+    for(int i=n.length()-1; i>=0; i--)
+    {
+        decimal += cnt*(n[i] - '0');
+        cnt *= a;
+    }
+
+    //10진수 -> b진수
+    int Bnum[15];
+    int i=0;
+    while(true)
+    {
+        if(decimal < b)
+        {
+            Bnum[i++] =  decimal;
+            break;
+        }
+        Bnum[i++] = decimal % b;
+        decimal /= b;
+    }
+
+
+    for(int j=i - 1; j>=0; j--)
+        cout << Bnum[j];
+    return 0;
+}
