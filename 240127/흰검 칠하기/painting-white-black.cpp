55ffcea63d030844c592f315e89f877abe983710
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-27 13:50:32 +0900

    Title: 흰검 칠하기, Time: 10ms, Memory: 1MB, Status: Passed - CodeTree

--- 240127/흰검 칠하기/painting-white-black.cpp
+++ 240127/흰검 칠하기/painting-white-black.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 #define MAXSIZE 200000
 int main() {
+    //변수 선언
     int n;
     int curPoint = MAXSIZE/2;
-    int blackCnt[MAXSIZE] = {0};
+    int blackCnt[MAXSIZE] = {0}; //색칠 횟수
     int whiteCnt[MAXSIZE] = {0};
-    char color[MAXSIZE] = {0};
+    char color[MAXSIZE] = {0}; //타일 색
 
+    //입력
     cin >> n;
     for(int i=0; i<n; i++)
     {
@@ -19,27 +21,26 @@ int main() {
         if(dir == 'L')
         {
             dist *= -1;
-
             for(int j=curPoint; j>curPoint + dist; j--)
             {
                 whiteCnt[j]++;
                 color[j] = 'W';
             }
-            dist += 1;
+            dist += 1; //마지막으로 칠한 타일 위에 멈추기
         }
         else
         {
-
             for(int j=curPoint; j<curPoint+dist; j++)
             {
                 blackCnt[j]++;
                 color[j] = 'B';
             }
-            dist -= 1;
+            dist -= 1; //마지막으로 칠한 타일 위에 멈추기
         }
-        curPoint += dist;
+        curPoint += dist; //이동
     }
 
+    //최종 타일 색 count
     int black = 0;
     int white = 0;
     int gray = 0;
@@ -53,6 +54,7 @@ int main() {
             black++;
     }
 
+    //출력
     cout << white << " " << black << " " << gray;
 
     return 0;
