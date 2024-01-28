e5a77b959215ef21c6a4d7609bab10d5f5863149
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-28 13:13:49 +0900

    Title: 사각형의 총 넓이 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240128/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //변수 선언
+    int n;
+    int arr[200][200] = {0};
+
+    //입력 및 사각형 표시
+    cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        int x1, x2, y1, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        for(int j=x1; j<x2; j++)
+        {
+            for(int k=y1; k<y2; k++)
+            {
+                arr[j][k]++;
+            }
+        }
+    }
+
+    //넓이 계산
+    int s = 0;
+    for(int j=0; j< 200; j++)
+    {
+        for(int k=0; k<200; k++)
+        {
+            if(arr[j][k] > 0)
+                s++;
+        }
+    } 
+
+    //출력
+    cout << s;
+    return 0;
+}
