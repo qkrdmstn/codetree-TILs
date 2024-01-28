195eca03690d8d90ba64687d9af3823fa3ab9623
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-28 13:28:19 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240128/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
+++ 240128/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -2,43 +2,36 @@
 using namespace std;
 
 int main() {
-    int ax1, ay1, ax2, ay2;
-    int bx1, by1, bx2, by2;
-    int mx1, my1, mx2, my2;
+    //변수 선언
+    int x1[3], x2[3];
+    int y1[3], y2[3];
     int arr[2000][2000] = {0};
 
-    cin >> ax1 >> ay1 >> ax2 >> ay2;
-    cin >> bx1 >> by1 >> bx2 >> by2;
-    cin >> mx1 >> my1 >> mx2 >> my2;
+    //입력
+    for(int i=0; i<3; i++)
+    {
+        cin >> x1[i] >> y1[i];
+        cin >> x2[i] >> y2[i];
 
-    ax1 += 1000; ay1 += 1000; ax2 += 1000; ay2 += 1000;
-    bx1 += 1000; by1 += 1000; bx2 += 1000; by2 += 1000;
-    mx1 += 1000; my1 += 1000; mx2 += 1000; my2 += 1000;
+        x1[i] += 1000;
+        x2[i] += 1000;
+        y1[i] += 1000;
+        y2[i] += 1000;
+    }
     
-    for(int i=ax1; i<ax2; i++)
+    //사각형 그리기
+    for(int i=0; i<3; i++)
     {
-        for(int j=ay1; j<ay2; j++)
+        for(int j=x1[i]; j<x2[i]; j++)
         {
-            arr[i][j]=1;
+            for(int k=y1[i]; k<y2[i]; k++)
+            {
+                a[j][k] = i+1;
+            }
         }
     }
 
-    for(int i=bx1; i<bx2; i++)
-    {
-        for(int j=by1; j<by2; j++)
-        {
-            arr[i][j]=2;
-        }
-    }
-
-    for(int i=mx1; i<mx2; i++)
-    {
-        for(int j=my1; j<my2; j++)
-        {
-            arr[i][j]=3;
-        }
-    }
-
+    //넓이 계산 -> 1번째, 2번째 사각형만 count
     int s = 0;
     for(int i=0; i<2000; i++)
     {
@@ -49,6 +42,7 @@ int main() {
         }
     }
 
+    //출력
     cout << s;
     return 0;
 }
