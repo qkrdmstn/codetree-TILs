851b4503c0a77881ace1c56c687e9db366bc8def
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-30 16:16:53 +0900

    Title: 선두를 지켜라 3, Time: 15ms, Memory: 7MB, Status: Passed - CodeTree

--- 240130/선두를 지켜라 3/keep-the-lead-3.cpp
+++ 240130/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -12,12 +12,15 @@ int Rank(int a, int b)
 }
 
 int main() {
+    //변수 선언
     int n, m;
     int a[1000001] = {0};
     int b[1000001] = {0};
-    int timeA = 1, timeB = 1;
+    int timeA = 1, timeB = 1; //현재 시간
 
+    
     cin >> n >> m;
+    //A의 움직임 저장
     for(int i=0; i<n; i++)
     {
         int v, t;
@@ -28,6 +31,7 @@ int main() {
         timeA += t;
     }
 
+    //B의 움직임 저장
     for(int i=0; i<m; i++)
     {
         int v, t;
@@ -40,7 +44,6 @@ int main() {
 
     int cnt = 0;
     int first = 0; //A: 1, B: 2, A&B = 3
-
     for(int i=1; i<timeA; i++)
     {
         if(first == 0)
@@ -50,12 +53,14 @@ int main() {
             continue;
         }
 
-        int before = first;
+        //1등의 조합이 바뀐 횟수를 count
+        int before = first; 
         first = Rank(a[i], b[i]);
         if(before != first)
             cnt++;
     }
 
+    //출력
     cout << cnt;
     return 0;
 }
