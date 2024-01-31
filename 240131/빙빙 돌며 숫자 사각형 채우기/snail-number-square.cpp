6f6088e4fd704d146558951cb74aa0e2f3b9cecf
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-01-31 17:36:09 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240131/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int n,m;
+bool InRange(int r, int c)
+{
+    return (r >=0 && r < n && c >= 0 && c < m);
+}
+
+int main() {
+    int arr[100][100] = {0};
+    bool visit[100][100] = {false};
+
+    int dr[4] = {-1, 0, 1, 0};
+    int dc[4] = {0, 1, 0, -1};
+    int dir = 1;
+    int cnt = 2;
+    int r = 0, c = 0;
+
+    arr[0][0] = 1;
+    visit[0][0] = true;
+
+    cin >> n >> m;
+    for(int i=0; i<n*m - 1; i++)
+    {
+        int nr = r + dr[dir];
+        int nc = c + dc[dir];
+
+        if(!InRange(nr, nc) || visit[nr][nc])
+        {
+            dir++;
+            dir %= 4;
+        }
+        
+        r = r + dr[dir];
+        c = c + dc[dir];
+
+        visit[r][c] = true;
+        arr[r][c] = cnt;
+        cnt++;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+            cout << arr[i][j] << " ";
+        cout << endl;
+    }
+    return 0;
+}
