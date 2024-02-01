6a514e97d6fb8616e350e3733a90462d21556c91
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-01 17:40:17 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240201/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.cpp
+++ 240201/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.cpp
@@ -9,30 +9,34 @@ int abs(int a)
 }
 
 int main() {
+    //변수 선언
     int n;
     int num[100] = {0};
     int max = 0;
 
+    //입력
     cin >> n;
     for(int i=0; i<n; i++)
         cin >> num[i];
     
-    for(int i=0; i<n; i++)
+    
+    for(int i=0; i<n; i++) //i번째 수와 j번째 수의 합
     {
         int sum = 0;
         for(int j=0; j<n; j++)
         {
-            if(i==j)
+            if(i==j) //자기 자신 skip
                 continue;
-            if(abs(i-j) <= 1)
+            if(abs(i-j) <= 1) //i와 j의 거리가 1이하면 인접
                 continue;
             
             sum = num[i] + num[j];
-            if(sum > max)
+            if(sum > max) //최대값 계산
                 max = sum;
         }
     }
 
+    //최대값 출력
     cout << max;
     return 0;
 }
