4a6d2e035bcd84f53484938bfd53bc41c4b36f97
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-01 22:14:21 +0900

    Title: 특정 수와 근접한 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240201/특정 수와 근접한 합/sum-close-to-particular-number.cpp
+++ 240201/특정 수와 근접한 합/sum-close-to-particular-number.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int s, n;
 int arr[100] = {0};
 
-int Difference(int a)
+int Difference(int a) //s와의 차이 계산
 {
     int result = s-a;
     if(result < 0)
@@ -21,24 +21,25 @@ int main() {
     for(int i=0; i<n; i++)
         cin >> arr[i];
     
-    for(int i=0; i<n; i++)
+    for(int i=0; i<n; i++) //서로 다른 두 i, j 찾기
     {
         for(int j=i+1; j<n; j++)
         {
             int sum = 0;
             for(int k=0; k<n; k++)
             {
-                if(k == i || k == j)
+                if(k == i || k == j) //i와 j번째 수를 제외
                     continue;
-                sum += arr[k];
+                sum += arr[k]; //나머지 더하기
             }
 
             int diff = Difference(sum);
-            if(diff < minDiff)
+            if(diff < minDiff) //차이가 최소
                 minDiff = diff;
         }
     }
 
+    //출력
     cout << minDiff;
     return 0;
 }
