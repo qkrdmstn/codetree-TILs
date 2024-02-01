1713a8427a2bef1cac141833263b51b1f838c7bf
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-01 14:24:31 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240201/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240201/체크판위에서 2/on-the-checkboard-2.cpp
@@ -15,9 +15,9 @@ int main() {
             cin >> grid[i][j];
     }
 
-    for(int i=1; i<=r; i++) //첫 번째 점프 지점 (i,j)
+    for(int i=1; i<r; i++) //첫 번째 점프 지점 (i,j)
     {
-        for(int j=1; j<=c; j++)
+        for(int j=1; j<c; j++)
         {
             if(grid[1][1] == grid[i][j]) //출발지와 색이 달라야 함.
                 continue;
@@ -28,11 +28,11 @@ int main() {
             {
                 for(int l=curC + 1; l<c; l++)
                 {
-                    if(grid[curR][curC] == grid[k][l])
+                    if(grid[curR][curC] == grid[k][l]) //첫 번째 지점과 색이 같음
                         continue;
-                    if(grid[k][l] == grid[r][c])
+                    if(grid[k][l] == grid[r][c]) //도착지점과 색이 같음
                         continue;
-                    
+
                     cnt++;
                 }
             }
