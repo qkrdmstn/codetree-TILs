eed3b0c276fe6ec52b5c96db28f2050aa58984b0
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-01 21:43:14 +0900

    Title: 숨은 단어 찾기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240201/숨은 단어 찾기 2/find-hidden-words-2.cpp
+++ 240201/숨은 단어 찾기 2/find-hidden-words-2.cpp
@@ -2,9 +2,10 @@
 #include <string>
 using namespace std;
 
+//변수 선언
 int n, m;
 string str[51];
-int dc[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+int dc[8] = {0, 1, 1, 1, 0, -1, -1, -1}; //8방향 dx, dy
 int dr[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
 
 bool InRange(int r, int c)
@@ -18,47 +19,46 @@ int CheckLee(int r, int c)
     for(int dir=0; dir<8; dir++)
     {
         string temp;
-        int nr1 = r + dr[dir];
+        int nr1 = r + dr[dir]; //현재 방향의 2번째 위치
         int nc1 = c + dc[dir];
-        int nr2 = nr1 + dr[dir];
+        int nr2 = nr1 + dr[dir]; //현재 방향의 3번째 위치
         int nc2 = nc1 + dc[dir];
 
-        if(!InRange(nr1, nc1) || !InRange(nr2, nc2))
+        if(!InRange(nr1, nc1) || !InRange(nr2, nc2)) //범위 확인
             continue;
 
-        char c1 = str[r][c];
-        char c2 = str[nr1][nc1];
-        char c3 = str[nr2][nc2];
+        char c1 = str[r][c]; //현재 위치 문자
+        char c2 = str[nr1][nc1]; //2번째 문자
+        char c3 = str[nr2][nc2]; //3번째 문자
         
         temp +=c1;
         temp +=c2;
         temp +=c3;
 
-        if(temp == "LEE")
+        if(temp == "LEE") //LEE라면 count
             cnt++;
     }
     return cnt;
 }
 
-int main() {
-
+int main() 
+{
+    //입력
     cin >> n >> m;
     for(int i=0; i<n; i++)
         cin >> str[i];
     
-    char a = str[2][0];
-    char b = str[2][1];
-    char c = str[2][2];
-
+    //Count
     int cnt = 0;
     for(int i=0; i<n; i++)
     {
         for(int j=0; j<m; j++)
         {
-            cnt += CheckLee(i,j);
+            cnt += CheckLee(i,j); //모든 위치에 대해 check
         }
     }
 
+    //출력
     cout << cnt;
     return 0;
 }
