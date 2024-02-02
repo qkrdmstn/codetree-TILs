547d263ea2bc11a8023e9c7ccb5d5dcfcd5ccee5
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-03 00:50:47 +0900

    Title: G or H 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240203/G or H 2/G-or-H-2.cpp
+++ 240203/G or H 2/G-or-H-2.cpp
@@ -8,14 +8,9 @@ struct People
     char c;
 };
 
-bool Compare(People a, People b)
+bool Compare(People a, People b) //정렬
 {
     return a.x < b.x;
-}
-
-bool InRange(int a, int b, People p)
-{
-    return (p.x >= a && p.x <= b);
 }
 
 int main() {
@@ -28,32 +23,31 @@ int main() {
     for(int i=0; i<n; i++)
         cin >> people[i].x >> people[i].c;
 
-    sort(people, people + n, Compare);
+    sort(people, people + n, Compare); //위치 오름차순 정렬
 
     int maxSize = 0;
-    for(int i=0; i<n; i++) //칸수 
+    for(int i=0; i<n; i++) //칸수 (index)
     {
-        for(int j=0; j<n-i; j++) //범위
+        for(int j=0; j<n-i; j++) //시작점 j
         {
             int cntG = 0;
             int cntH = 0;
-            for(int k=j; k<=j+i; k++)
+            for(int k=j; k<=j+i; k++) //i칸수만큼 구간 순회
             {
-                if(people[k].c == 'G')
+                if(people[k].c == 'G') //G, H 개수 count
                     cntG++;
                 else if(people[k].c == 'H')
                     cntH++;
             }
-            if(cntG == cntH || cntG == 0 || cntH == 0)
+            if(cntG == cntH || cntG == 0 || cntH == 0) //조건
             {
-                if(maxSize < people[j+i].x - people[j].x)
-                {
+                if(maxSize < people[j+i].x - people[j].x) //최댓값
                     maxSize = people[j+i].x - people[j].x;
-                }
             }
         }
     }
 
+    //출력
     cout << maxSize;
     return 0;
 }
