0bb2f3843710afb8f97876ad9b555e99e3bccf78
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-03 15:36:55 +0900

    Title: 개발자의 능력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240203/개발자의 능력 2/ability-of-developer-2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool isDuplicate(int a, int b, int c, int d)
+{
+    return (a==b || a==c || a == d || b == c || b == d || c == d);
+}
+
+int main() {
+    int ability[6];
+    int totalSum = 0;
+    int minDiff = 1000000;
+
+    for(int i=0; i<6; i++)
+    {
+        cin >> ability[i];
+        totalSum += ability[i];
+    }
+
+    for(int i=0; i<6; i++)
+    {
+        for(int j=0; j<6; j++)
+        {
+            for(int k=0; k<6; k++)
+            {
+                for(int l=0; l<6; l++)
+                {
+                    if(isDuplicate(i, j, k, l)) continue;
+                    int sum1 = ability[i] + ability[j];
+                    int sum2 = ability[k] + ability[l];
+                    int sum3 = totalSum - (sum1 + sum2);
+
+                    int minSum = min(sum1, min(sum2, sum3));
+                    int maxSum = max(sum1, max(sum2, sum3));
+                    int diff = maxSum - minSum;
+                    
+                    if(diff < minDiff)
+                        minDiff = diff;
+                }
+            }
+        }
+    }
+
+    cout << minDiff;
+    return 0;
+}
