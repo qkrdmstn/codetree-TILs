e978745af8e5b87672740f31c12fb861939d3b6d
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-03 15:46:49 +0900

    Title: 개발팀의 능력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240203/개발팀의 능력/the-capabilities-of-the-development-team.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool isDuplicate(int a, int b, int c) //중복 확인
+{
+    return (a==b || a==c || b==c);
+}
+
+int main() {
+    //변수 선언
+    int ability[5];
+    int totalSum = 0;
+    int minDiff = 1000000;
+
+    //입력
+    for(int i=0; i<5; i++)
+    {
+        cin >> ability[i];
+        totalSum += ability[i];
+    }
+
+    for(int i=0; i<5; i++) //팀1 (i, j)
+    {
+        for(int j=0; j<5; j++)
+        {
+            for(int k=0; k<5; k++) //팀2 (k)
+            {
+                if(isDuplicate(i,j,k)) continue; //팀원 중복 확인
+
+                int sum1 = ability[i] + ability[j];
+                int sum2 = ability[k];
+                int sum3 = totalSum - (sum1 + sum2);
+
+                if(isDuplicate(sum1, sum2, sum3)) continue;
+                
+                int minSum = min(sum1, min(sum2, sum3));
+                int maxSum = max(sum1, max(sum2, sum3));
+                int diff = maxSum - minSum;
+
+                if(diff < minDiff)
+                    minDiff = diff;
+            }
+        }
+    }
+
+    //출력
+    cout << minDiff;
+    return 0;
+}
