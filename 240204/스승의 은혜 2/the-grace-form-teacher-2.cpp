08c124c86e3976105b67f438f76e1143e15dfffa
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-04 14:19:46 +0900

    Title: 스승의 은혜 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240204/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, b;
+    int price[1000];
+
+    cin >> n >> b;
+    for(int i=0; i<n; i++)
+        cin >> price[i];
+
+    sort(price, price + n);
+
+    int i=0;
+    bool flag = true;
+    while(b > 0)
+    {
+        b -= price[i];
+        if(b <= 0 && flag)
+        {
+            b += price[i]/2;
+            flag = false;
+        }
+        i++;
+    }
+
+    cout << i-1;
+    return 0;
+}
