99b431012b40155d168a1b8b10278fca5a991077
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-05 15:49:01 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240205/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 240205/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -23,41 +23,33 @@ int main() {
     //입력
     cin >> N >> B;
     for(int i=0; i<N; i++)
-    {
         cin >> students[i].present >> students[i].delivCost;
-        students[i].sum = students[i].present + students[i].delivCost;
-    }
-    
-    sort(students, students + N, Compare); //배송비 + 선물 가격 합계로 오름차순 정렬
 
-    int i=0;
-    bool discount = false;
-    while(B > 0)
+    int maxCnt = 0;
+    for(int i=0; i<N; i++) //할인 받을 학생
     {
-        B -= students[i].sum; //합계 가격이 낮은 것 순서대로 구매
-        students[i].buy = true;
-
-        if(!discount && B < 0) //예산 초과
+        for(int j=0; j<N; j++)
         {
-            B += students[i].sum; //마지막에 산 선물 환불
-
-            for(int j=0; j<N; j++) //할인된 가격 재설정
-                students[j].sum = students[j].present/2 + students[j].delivCost;
-            
-            sort(students, students + N, Compare); //할인된 가격 순으로 오름차순 정렬
-
-            for(int j=0; j<N; j++)
-            {
-                if(!students[j].buy) //아직 구매하지 않은 것 중
-                    B -= students[j].sum; //가장 앞에 있는 것 구매    
-            }
-            discount = true;
+            if(j==i)
+                students[j].sum = students[j].present/2 + students[j].delivCost; //할인
+            else
+                students[j].sum = students[j].present + students[j].delivCost;                
         }
 
-        if(B >= 0)
-            i++;
+        sort(students, students + N, Compare); //배송비 + 선물 가격 합계로 오름차순 정렬
+
+        int k=0;
+        while(B >= 0)
+        {
+            B -= students[k].sum;
+            if(B >= 0) //구매 성공
+                k++;
+        }
+
+        if(k > maxCnt)
+            maxCnt = k;
     }
 
-    cout << i;
+    cout << maxCnt;
     return 0;
 }
