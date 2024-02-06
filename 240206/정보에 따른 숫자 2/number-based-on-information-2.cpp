1084ee4ac9f6b17bc813377d4db0c99443f76989
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-06 13:30:40 +0900

    Title: 정보에 따른 숫자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/정보에 따른 숫자 2/number-based-on-information-2.cpp
+++ 240206/정보에 따른 숫자 2/number-based-on-information-2.cpp
@@ -8,20 +8,20 @@ int pos[100] = {0};
 bool isSpecial(int x)
 {
     int d1 = 1000, d2 = 1000;
-    for(int i=0; i<T; i++){
+    for(int i=0; i<T; i++){ //모든 알파벳 순회
         int dist1 = 1000;
         int dist2 = 1000;
 
-        if(c[i] == 'S')
+        if(c[i] == 'S') //거리 계산
             dist1 = abs(pos[i] - x);
         else if(c[i] == 'N')
             dist2 = abs(pos[i] - x);
 
-        if(dist1 < d1) d1 = dist1;
+        if(dist1 < d1) d1 = dist1; //거리 최소
         if(dist2 < d2) d2 = dist2;
     }
 
-    if(d1 <= d2)
+    if(d1 <= d2) //조건 만족
         return true;
     else
         return false;
@@ -33,13 +33,14 @@ int main() {
     for(int i=0; i<T; i++)
         cin >> c[i] >> pos[i];
 
-
+    //계산
     int cnt = 0;
     for(int i=a; i<=b; i++){
         if(isSpecial(i))
             cnt++;
     }
 
+    //출력
     cout << cnt;
     return 0;
 }
