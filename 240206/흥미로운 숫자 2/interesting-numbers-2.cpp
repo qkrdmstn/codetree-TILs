b02da35657b4dfb3d41758bdc44377186b8a8df6
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-06 13:08:06 +0900

    Title: 흥미로운 숫자 2, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 240206/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -6,31 +6,34 @@ bool isInteresting(int x)
     int digit[10] = {0};
     int i=0;
     while(x>0){
-        digit[x%10]++;
+        digit[x%10]++; //0-9 count
         x /=10;
-        i++;
+        i++; //자리수
     }
 
     bool flag = false;
     for(int j=0; j<10; j++){
-        if(i-1 == digit[j])
+        if(i-1 == digit[j]) //자리수 - 1개만큼 숫자가 있어야 함.
             return true;
     }
-
-    return false;;
+    return false;
 }
 
 int main() {
+    //변수 선언
     int x, y;
 
+    //입력
     cin >> x >> y;
 
+    //계산
     int cnt = 0;
     for(int i=x; i<=y; i++){
         if(isInteresting(i))
             cnt++;
     }
-
+    
+    //출력
     cout << cnt;
     return 0;
 }
