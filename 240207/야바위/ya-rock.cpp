e2a89178e49aea9f2a5cacfeca0cb5466b64f032
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-07 21:06:33 +0900

    Title: 야바위, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240207/야바위/ya-rock.cpp
+++ 240207/야바위/ya-rock.cpp
@@ -15,6 +15,7 @@ int main() {
     int b[100] = {0};
     int c[100] = {0};
     
+    //입력
     cin >> n;
     for(int i=0; i<n; i++)
         cin >> a[i] >> b[i] >> c[i];
@@ -25,14 +26,15 @@ int main() {
         cup[i] = 1;
         int score = 0;
         for(int j=0; j<n; j++){
-            Change(a[j], b[j], cup);
-            if(cup[c[j]] == 1)
+            Change(a[j], b[j], cup); //a컵 b컵 교환
+            if(cup[c[j]] == 1) //c번 컵 확인
                 score++;
         }
-        if(score > maxScore)
+        if(score > maxScore) //최대 점수
             maxScore = score;
     }
 
+    //출력
     cout << maxScore;
     return 0;
 }
