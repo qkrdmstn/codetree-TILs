f502ff96a58c0f410e9170e160deddcf87cd2890
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-11 13:51:23 +0900

    Title: 독서실의 거리두기 5, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240211/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int n;
+string arr;
+int GetDist(string str){
+
+    int minDist = n;
+    for(int i=0; i<n; i++){
+        if(str[i] == '1'){
+            int dist = 1;
+            while(str[i+dist] != '1'){
+                dist++;
+            }
+            if(dist < minDist)
+                minDist = dist;
+        }
+    }
+
+    return minDist;
+}
+
+int main() {
+    cin >> n;
+    cin >> arr;
+
+    int maxDist = 0;
+    for(int i=0; i<n; i++){
+        string str = arr;
+        if(str[i] == '1')   continue;
+        str[i] = '1';
+        
+        int minDist = GetDist(str);
+        if(minDist > maxDist)
+            maxDist = minDist;
+    }
+
+    cout << maxDist;
+    return 0;
+}
