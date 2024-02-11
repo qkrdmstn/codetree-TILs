5ae25d9c95baad40579097059f86748f22fae31c
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-11 15:12:50 +0900

    Title: 훌륭한 점프, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240211/훌륭한 점프/great-jump.cpp
+++ 240211/훌륭한 점프/great-jump.cpp
@@ -6,6 +6,8 @@ int n, k;
 int arr[100] = {0};
 bool IsPossible(int max)
 {
+    //max 값보다 작은 값을 가진 index를 저장
+    //이후, index의 차이(dist)가 k보다 작은지 확인
     int indices[100] = {0};
     int cnt = 0;
 
@@ -34,8 +36,10 @@ int main() {
             maxArr = arr[i];
     }
 
+    //최대값을 a로 가정. 양 끝 돌을 지나야 하기 때문에
+    //for문의 시작을 다음과 같이 설정
     int minimax = 100;
-    for(int a=max(arr[0], arr[n-1]); a<=maxArr; a++){
+    for(int a=max(arr[0], arr[n-1]); a<=maxArr; a++){ 
         int max = 100;
         if(IsPossible(a))
             max = a;
@@ -43,6 +47,7 @@ int main() {
             minimax = max;
     }
 
+    //출력
     cout << minimax;
     return 0;
 }
