8ac2d9f0769086f19344436f941182dece0c8097
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-12 13:59:43 +0900

    Title: 초기 수열 복원하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240212/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int arr[1000] = {0};
+int sum[1000] = {0};
+bool IsPossible(int a0)
+{
+    arr[0] = a0;
+    bool flag[1000] = {false};
+    for(int i=1; i<n; i++){
+        arr[i] = sum[i-1] - arr[i-1];
+        if(arr[i] > n || arr[i] < 1 || flag[arr[i]])
+            return false;
+        flag[arr[i]] = true;
+    }
+    
+    return true;
+}
+
+int main() {
+
+    cin >> n;
+    for(int i=0; i<n-1; i++)
+        cin >> sum[i];
+
+    for(int i=1; i<=n; i++){ //A0 값 i (수열의 시작)
+        if(IsPossible(i)){
+            for(int i=0; i<n; i++)
+                cout << arr[i] << " ";
+            return 0;
+        }
+    }
+    return 0;
+}
