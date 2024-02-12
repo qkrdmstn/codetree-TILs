907b3457512abe1e08167886a406128557e023dd
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-12 14:42:56 +0900

    Title: 언덕 깎기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240212/언덕 깎기/hill-cutting.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int height[1000] = {0};
+
+    //입력
+    cin >> n;
+    for(int i=0; i<n; i++)
+        cin >> height[i];
+
+    int minCost = 100000;
+    for(int i=0; i<=100; i++){
+        int cost = 0;
+        for(int j=0; j<n; j++){
+            if(height[j] > i+17)
+                cost += (height[j] - (i + 17))*(height[j] - (i + 17));
+            if(height[j] < i)
+                cost += (height[j] - i)*(height[j] - i);
+        }
+        if(cost < minCost)
+            minCost = cost;
+    }
+
+    cout << minCost;
+    return 0;
+}
