9f45a5d9bbea5facf9166cee1abc75134d7d364f
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-13 14:10:43 +0900

    Title: 최대 H 점수 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240213/최대 H 점수 2/maximum-h-score-2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int n, l;
+int arr[100] = {0};
+bool IsPossible(int temp2[], int a) //H 점수가 가능한 원소인가.
+{
+    int cnt = 0;
+    for(int i=0; i<n; i++){
+        if(temp2[i] >= a)
+            cnt++;
+    }
+
+    if(cnt >= a)
+        return true;
+    else
+        return false;
+}
+
+int main() {
+
+    cin >> n >> l;
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+
+    int score = 0;
+    for(int i=0; i<n; i++){
+        int temp[100] = {0};
+        for(int k=0; k<n; k++)
+            temp[k] = arr[k];
+
+        int cnt = 0;
+        int flag = false;
+        while(true){
+
+            if(cnt > l) break;
+            if(IsPossible(temp, temp[i])){
+                flag = true;
+                break;
+            } 
+
+            for(int j=0; j<n; j++){
+                if(temp[j] == temp[i]-1){
+                    temp[j]++;
+                    break;
+                } 
+            }
+            cnt++;
+        }
+        if(cnt <= l && flag){
+            if(temp[i] > score)
+                score = temp[i];
+        }
+    }
+
+    cout << score;
+    return 0;
+}
