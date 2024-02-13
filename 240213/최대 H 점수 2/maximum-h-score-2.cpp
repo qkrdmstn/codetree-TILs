d5103ca134bb167fa2a1231fc387be227acd4823
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-13 14:16:40 +0900

    Title: 최대 H 점수 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240213/최대 H 점수 2/maximum-h-score-2.cpp
+++ 240213/최대 H 점수 2/maximum-h-score-2.cpp
@@ -27,7 +27,7 @@ int main() {
     }
 
     int score = 0;
-    for(int i=0; i<max; i++){
+    for(int i=0; i<max; i++){ //H점수 가정
         int temp[100] = {0};
         for(int k=0; k<n; k++)
             temp[k] = arr[k];
@@ -35,26 +35,27 @@ int main() {
         int cnt = 0;
         int flag = false;
         while(true){
-            if(cnt > l) break;
-            if(IsPossible(temp, i)){
+            if(cnt > l) break; //더한 횟수가 l보다 크면 break
+            if(IsPossible(temp, i)){ //H점수가 계산 가능하면 break
                 flag = true;
                 break;
             } 
 
-            for(int j=0; j<n; j++){
+            for(int j=0; j<n; j++){ //h점수보다 1 낮은 수 더하기
                 if(temp[j] == i-1){
                     temp[j]++;
                     break;
                 } 
             }
-            cnt++;
+            cnt++; //더한 횟수 count
         }
-        if(cnt <= l && flag){
-            if(i > score)
-                score = i;
+        if(cnt <= l && flag){ //더한 횟수가 l번 이내이고, H점수 계산 가능
+            if(i > score) //점수 최대값
+                score = i; 
         }
     }
 
+    //출력
     cout << score;
     return 0;
 }
