a6185d678817fd8e110a01e5125355f551aa55bb
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-13 12:46:09 +0900

    Title: 구간 잘 나누기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240213/구간 잘 나누기/divide-sections-well.cpp
+++ 240213/구간 잘 나누기/divide-sections-well.cpp
@@ -2,29 +2,32 @@
 using namespace std;
 
 int main() {
+    //변수 선언
     int n, m;
     int arr[100] = {0};
 
+    //입력
     cin >> n >> m;
     for(int i=0; i<n; i++)
         cin >> arr[i];
 
     int max = 1;
-    while(true){
+    while(true){ //최대값을 가정하고 
         int index = 0;
         for(int j=0; j<m; j++){
             int sum=0;
-            while(true){
+            while(true){ //합이 max를 안 넘는 범위에서 구간 만들기
                 if(sum + arr[index] > max)
                     break;
                 sum += arr[index++];
             }
         }
-        if(index > n-1)
+        if(index > n-1) //끝까지 m번만에 나눠졌으면 break;
             break;
         max++;
     }
 
+    //출력
     cout << max;
     
     return 0;
