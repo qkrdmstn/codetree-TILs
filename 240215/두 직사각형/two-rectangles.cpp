89a0d06d38389ea38d0bcc4e7fa674f374ecbeb9
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-15 10:40:47 +0900

    Title: 두 직사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240215/두 직사각형/two-rectangles.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int x1, y1, x2, y2;
+int a1, b1, a2, b2;
+
+bool IsOverlapLine(int a, int b, int c, int d)
+{
+    if(a<c && b<c)
+        return false;
+    else if(c < a && d < a)
+        return false;
+
+    return true;
+}
+
+bool IsOverlapAABB()
+{
+    if(IsOverlapLine(x1, x2, a1, a2) && IsOverlapLine(y1, y2, b1, b2))
+        return true;
+    else    
+        return false;
+}
+
+int main() {
+
+    cin >> x1 >> y1 >> x2 >> y2;
+    cin >> a1 >> b1 >> a2 >> b2;
+
+    if(IsOverlapAABB())
+        cout << "overlapping";
+    else
+        cout << "nonoverlaping";
+    return 0;
+}
