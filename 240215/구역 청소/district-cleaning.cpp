7255a099eccb76f5acaae31b7af15035300df1e6
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-15 11:09:29 +0900

    Title: 구역 청소, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240215/구역 청소/district-cleaning.cpp
+++ 240215/구역 청소/district-cleaning.cpp
@@ -16,12 +16,14 @@ int main() {
     cin >> a >> b >> c >> d;
 
     if(IsOverlap()){
-        if(a<c && d<b)
-            cout << abs(a-b);
-        else if((c<a && b<d))
+        if(a<=c && d<=b)
             cout << abs(c-d);
-        else
+        else if(c<=a && b<=d)
+            cout << abs(a-b);
+        else if(a < c)
             cout << abs(a-d);
+        else if(c < a)
+            cout << abs(c-b);
     }
     else
         cout << abs(a-b) + abs(c-d);
