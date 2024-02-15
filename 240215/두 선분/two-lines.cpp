b4b2a144788cec1707eb360830cb03cefaabf84c
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-15 10:33:14 +0900

    Title: 두 선분, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240215/두 선분/two-lines.cpp
+++ 240215/두 선분/two-lines.cpp
@@ -5,8 +5,8 @@ int x1, x2, x3, x4;
 bool IsOverlap()
 {
     if(x1 < x3 && x2 < x3)
-        return false;
-    else if(x3 < x1 && x4 < x2)
+        return false; 
+    else if(x3 < x1 && x4 < x1)
         return false;
     
     return true;
