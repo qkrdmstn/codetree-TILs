e9ae9067422ef628816855b802be791f95491546
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-16 13:04:30 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240216/코딩톡/coding-talk.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m, p;
+char c[101];
+int u[101];
+
+bool IsRead(char name)
+{
+    for(int i=p; i<=m; i++){
+        if(c[i] == name)
+            return true;
+    }
+    return false;
+}
+
+int main() {
+
+    cin >> n >> m >> p;
+    for(int i=1; i<=m; i++)
+        cin >> c[i] >> u[i];
+
+    for(int i=65; i<65+n; i++){
+        if(!IsRead(i)){
+            char name = i;
+            cout << name << " ";
+        }
+    }
+    return 0;
+}
