6f6c31107b74d1d2934b74002c097705c8e615d5
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-16 12:53:33 +0900

    Title: 달리자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240216/달리자/run-and-run.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int a[100] = {0};
+    int b[100] = {0};
+
+    //입력
+    cin >> n;
+    for(int i=0; i<n; i++)
+        cin >> a[i];
+    for(int i=0; i<n; i++)
+        cin >> b[i];
+
+    int cost = 0;
+    for(int i=n-1; i>=1; i--){
+        if(a[i] == b[i]) continue;
+
+        while(a[i] != b[i]){
+            a[i-1]--;
+            a[i]++;
+            cost++;
+        }
+    }
+
+    cout << cost;
+    return 0;
+}
