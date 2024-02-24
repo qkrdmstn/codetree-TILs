4ac99ee0ea7e2c1fb1016e6ace53bc1d7aa46c7c
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2024-02-24 15:11:08 +0900

    Title: X 달리기, Time: 1074ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240224/X 달리기/x-run.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    int curV = 1;
+    int curPos = 0;
+
+    cin >> x;
+    int time = 0;
+    while(curPos != x){
+        curPos += curV;
+
+        if(curPos < x/2)
+            curV++;
+        else if(curV == 1)
+            curV = curV;
+        else if(curPos >= x/2)
+            curV--;
+
+        time++;
+    }
+    
+    cout << time;
+
+    return 0;
+}
